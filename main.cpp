fd6f571303ed2e21f5d8b2bdf4f3e7637df5e587
Raylin Gloor <raylin@MacBook-Air-von-Raylin.local>
2025-10-23 23:29:10 +0200

    error handling

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ int main() {
     json result = ApiRequestWJsonAsClass::startQuery();
     cout << "Returned JSON:\n" << result << std::endl;
 
+    if (result.contains("current_weather")){
     cout << "das momentane Wetter beträgt " << result["current_weather"]["temperature"] << " °C" << endl;
-
+    } else {
+        cout << "Unkown API no weather Data displayed" << endl;
+    }
+    
     if (result.is_null()) {
         cerr << "No valid JSON returned." << endl;
         return 1;
